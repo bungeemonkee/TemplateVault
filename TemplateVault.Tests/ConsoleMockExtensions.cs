15e1e28709e8f383d8e21d84088aa13c24aceac7
David Love <david.love@doubleverify.com>
2022-03-04 14:59:13 -0500

    Vastly improved auth tests

--- TemplateVault.Tests/ConsoleMockExtensions.cs
+++ TemplateVault.Tests/ConsoleMockExtensions.cs
@@ -1,42 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Moq;
 using TemplateVault.Utilities;
 
 namespace TemplateVault.Tests
 {
+    [ExcludeFromCodeCoverage]
     public static class ConsoleMockExtensions
     {
-        public static void SetupReadValue(this Mock<IAbstractConsole> console, string value)
+        public static void SetupReadValue(this Mock<IAbstractConsole> console, params string?[] values)
         {
             console.Setup(x => x.Write(It.IsNotNull<string>(), It.IsNotNull<string>(), It.IsNotNull<string>()));
             console.Setup(x => x.ReadLine())
-                .Returns(value);
+                .Returns(new Queue<string?>(values).Dequeue);
         }
 
-        public static void SetupReadSecureValue(this Mock<IAbstractConsole> console, string value)
+        public static void SetupReadSecureValue(this Mock<IAbstractConsole> console, params string[] values)
         {
             console.Setup(x => x.Write(It.IsNotNull<string>(), It.IsNotNull<string>()));
             console.Setup(x => x.ReadKey(true))
-                .Returns(GetConsoleKeyQueue(value).Dequeue);
+                .Returns(GetConsoleKeyQueue(values).Dequeue);
             console.Setup(x => x.WriteLine());
         }
 
-        private static Queue<ConsoleKeyInfo> GetConsoleKeyQueue(string value)
+        private static Queue<ConsoleKeyInfo> GetConsoleKeyQueue(string[] values)
         {
             var queue = new Queue<ConsoleKeyInfo>();
-            foreach (var c in value)
+            foreach (var value in values)
             {
-                queue.Enqueue(new ConsoleKeyInfo(c, GetConsoleKey(c), false, false, false));
+                foreach (var c in value)
+                {
+                    queue.Enqueue(new ConsoleKeyInfo(c, GetConsoleKey(c), false, false, false));
+                }
+
+                queue.Enqueue(new ConsoleKeyInfo((char)13, ConsoleKey.Enter, false, false, false));
             }
 
-            queue.Enqueue(new ConsoleKeyInfo((char)13, ConsoleKey.Enter, false, false,false));
-            
             return queue;
         }
 
         private static ConsoleKey GetConsoleKey(char c)
         {
+            // special handling for backspace
+            if (c == '\b')
+            {
+                return ConsoleKey.Backspace;
+            }
+
             if (Enum.TryParse<ConsoleKey>(c.ToString().ToUpper(), out var val))
             {
                 return val;
--- TemplateVault.Tests/Vault/VaultAuthFactoryTests.cs
+++ TemplateVault.Tests/Vault/VaultAuthFactoryTests.cs
@@ -5,6 +5,17 @@ using NUnit.Framework;
 using TemplateVault.Utilities;
 using TemplateVault.Vault;
 using VaultSharp.V1.AuthMethods.AppRole;
+using VaultSharp.V1.AuthMethods.Azure;
+using VaultSharp.V1.AuthMethods.GitHub;
+using VaultSharp.V1.AuthMethods.GoogleCloud;
+using VaultSharp.V1.AuthMethods.JWT;
+using VaultSharp.V1.AuthMethods.Kerberos;
+using VaultSharp.V1.AuthMethods.Kubernetes;
+using VaultSharp.V1.AuthMethods.LDAP;
+using VaultSharp.V1.AuthMethods.Okta;
+using VaultSharp.V1.AuthMethods.RADIUS;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods.UserPass;
 
 namespace TemplateVault.Tests.Vault
 {
@@ -12,41 +23,71 @@ namespace TemplateVault.Tests.Vault
     public class VaultAuthFactoryTests
     {
         [Test]
+        public void GetSupportedAuthTypes()
+        {
+            var consoleMock = new Mock<IAbstractConsole>(MockBehavior.Strict);
+            
+            var factory = new VaultAuthFactory(consoleMock.Object);
+            var authTypes = factory.GetSupportedAuthTypes();
+            
+            Assert.IsNotEmpty(authTypes);
+            
+            consoleMock.VerifyAll();
+        }
+        
+        [Test]
         public void GetAuth_InvalidAuthType()
         {
-            var consoleMock = new Mock<IAbstractConsole>();
+            var consoleMock = new Mock<IAbstractConsole>(MockBehavior.Strict);
             
             var factory = new VaultAuthFactory(consoleMock.Object);
 
             Assert.Throws<InvalidOperationException>(() => factory.GetAuth("invalid type", null));
         }
-
+        
         [Test]
-        public void GetAuth_Approle()
+        [TestCase("approle", typeof(AppRoleAuthMethodInfo), new[] {"roleId"}, new [] {"secret"}, null)]
+        [TestCase("approle", typeof(AppRoleAuthMethodInfo), new[] {"roleId"}, new [] {"secret"}, "mount")]
+        [TestCase("azure", typeof(AzureAuthMethodInfo), new[] {"roleId", null, null, null, null}, new [] {"jwt"}, null)]
+        [TestCase("azure", typeof(AzureAuthMethodInfo), new[] {"roleId", null, null, null, null}, new [] {"jwt"}, "mount")]
+        [TestCase("github", typeof(GitHubAuthMethodInfo), null, new [] {"token"}, null)]
+        [TestCase("github", typeof(GitHubAuthMethodInfo), null, new [] {"token"}, "mount")]
+        [TestCase("gcp", typeof(GoogleCloudAuthMethodInfo), new[] {"roleId"}, new [] {"jwt"}, null)]
+        [TestCase("gcp", typeof(GoogleCloudAuthMethodInfo), new[] {"roleId"}, new [] {"jwt"}, "mount")]
+        [TestCase("jwt", typeof(JWTAuthMethodInfo), new[] {"roleId"}, new [] {"jwt"}, null)]
+        [TestCase("jwt", typeof(JWTAuthMethodInfo), new[] {"roleId"}, new [] {"jwt"}, "mount")]
+        [TestCase("kerbos", typeof(KerberosAuthMethodInfo), new[] {"username", "domain"}, new [] {"password"}, null)]
+        [TestCase("kerbos", typeof(KerberosAuthMethodInfo), new[] {"username", "domain"}, new [] {"password"}, "mount")]
+        [TestCase("kubernetes", typeof(KubernetesAuthMethodInfo), new[] {"roleId"}, new [] {"jwt"}, null)]
+        [TestCase("kubernetes", typeof(KubernetesAuthMethodInfo), new[] {"roleId"}, new [] {"jwt"}, "mount")]
+        [TestCase("ldap", typeof(LDAPAuthMethodInfo), new[] {"username"}, new [] {"password"}, null)]
+        [TestCase("ldap", typeof(LDAPAuthMethodInfo), new[] {"username"}, new [] {"password"}, "mount")]
+        [TestCase("okta", typeof(OktaAuthMethodInfo), new[] {"username"}, new [] {"password"}, null)]
+        [TestCase("okta", typeof(OktaAuthMethodInfo), new[] {"username"}, new [] {"password"}, "mount")]
+        [TestCase("radius", typeof(RADIUSAuthMethodInfo), new[] {"username"}, new [] {"password"}, null)]
+        [TestCase("radius", typeof(RADIUSAuthMethodInfo), new[] {"username"}, new [] {"password"}, "mount")]
+        [TestCase("token", typeof(TokenAuthMethodInfo), null, new [] {"token"}, null)]
+        [TestCase("token", typeof(TokenAuthMethodInfo), null, new [] {"token"}, "mount")]
+        [TestCase("userpass", typeof(UserPassAuthMethodInfo), new[] {"username"}, new [] {"password"}, null)]
+        [TestCase("userpass", typeof(UserPassAuthMethodInfo), new[] {"username"}, new [] {"password"}, "mount")]
+        [TestCase("userpass", typeof(UserPassAuthMethodInfo), new[] {"username"}, new [] {"pass\bsword"}, null)]
+        public void GetAuth(string authType, Type expectedType, string?[]? values, string[]? secureValues, string mount)
         {
-            var consoleMock = new Mock<IAbstractConsole>();
-            consoleMock.SetupReadValue("username");
-            consoleMock.SetupReadSecureValue("password");
+            var consoleMock = new Mock<IAbstractConsole>(MockBehavior.Strict);
+            if (values != null)
+            {
+                consoleMock.SetupReadValue(values);
+            }
+
+            if (secureValues != null)
+            {
+                consoleMock.SetupReadSecureValue(secureValues);
+            }
 
             var factory = new VaultAuthFactory(consoleMock.Object);
-            var auth = factory.GetAuth("approle", null);
+            var auth = factory.GetAuth(authType, mount);
 
-            Assert.IsInstanceOf<AppRoleAuthMethodInfo>(auth);
-            
-            consoleMock.VerifyAll();
-        }
-
-        [Test]
-        public void GetAuth_Approle_Mount()
-        {
-            var consoleMock = new Mock<IAbstractConsole>();
-            consoleMock.SetupReadValue("username");
-            consoleMock.SetupReadSecureValue("password");
-
-            var factory = new VaultAuthFactory(consoleMock.Object);
-            var auth = factory.GetAuth("approle", "mount");
-
-            Assert.IsInstanceOf<AppRoleAuthMethodInfo>(auth);
+            Assert.IsInstanceOf(expectedType, auth);
             
             consoleMock.VerifyAll();
         }
